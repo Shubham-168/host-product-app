580236bfaa2454aa5d27911edbf8549030fbca32
shubham <shubham@gmail.com>
2025-06-20 18:12:47 +0530

    updating hamburgerMenu

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const HamburgerMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,9 +20,9 @@ const HamburgerMenu = () => {
       {isOpen && (
         <div className="absolute top-16 left-0 w-full bg-white shadow-md z-50 p-4">
           <ul className="flex flex-col space-y-4 text-gray-700 font-medium">
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <Link to="/" > <li>Home</li> </Link>
+            <Link to="/about" > <li>About</li> </Link>
+            <Link to="/contact" > <li>Contact</li> </Link>
           </ul>
         </div>
       )}
